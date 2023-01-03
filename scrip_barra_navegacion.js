d544b20fe429694f777b572d10fec74b3bc35d99
kuisx2 <facundomunho@gmail.com>
2023-01-03 20:01:13 -0300

    incluido el funcionamiento de boostrap js

--- scrip_barra_navegacion.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("navbar-toggler").addEventListener("click", function() {
-    let estado_menu_expand = document.getElementById("navbarNavAltMarkup").className;
-    if (estado_menu_expand == "collapse navbar-collapse"){
-        document.getElementById("navbarNavAltMarkup").className = "navbar-collapse"  
-    }else
-    document.getElementById("navbarNavAltMarkup").className = "collapse navbar-collapse";
-})
