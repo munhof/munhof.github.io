2070d51917e0e3c218507d340619d6d4592a7f3c
kuisx2 <facundomunho@gmail.com>
2023-01-03 11:48:43 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,6 +1,6 @@
-estado_nav = document.getElementsById("navbarNavAltMarkup").class
-console.log(estado_nav)
+estado_nav = document.getElementsById("navbarNavAltMarkup").className;
+console.log(estado_nav);
 
 document.getElementsByClassName(navbar-toggler).addEventListener("click", function(){
 
-})
+});
