13ca426d60ea1b6d77bbf66b641a346c7f10100b
munhof <munhofdev@gmail.com>
2023-01-18 02:22:16 -0300

    Cuerpo fomulario auto asdjustble
    se agrego el scrip : scrip-textarea-autoajustable.js
    el cual permite auto ajustar regiones de textarea según la cantidad de líneas que tenga

---
+++ gretel/scrips/scrip-textarea-autoadjustable.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("textarea_form").addEventListener("input", function(){
+    var num_lineas = document.getElementById("textarea_form").value.split(/\r\n|\r|\n/, -1).length;
+    console.log(num_lineas.toString()+"em")
+    document.getElementById("textarea_form").style.height = num_lineas.toString()+"em";
+    document.getElementById("textarea_label").style.cssText = "transform: translateY("+(-num_lineas-3).toString()+"em) scale(.9); transition: transform 0s, color 0s;"
+  });
